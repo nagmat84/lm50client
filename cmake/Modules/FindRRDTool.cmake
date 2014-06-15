152084e66e814913a5620eca4deb5191dd5226be
Matthias Nagel <matthias.h.nagel@gmail.com>
2014-06-15 14:16:41 +0200

    Fixed bug in cmake module for Debian.

--- cmake/Modules/FindRRDTool.cmake
+++ cmake/Modules/FindRRDTool.cmake
@@ -1,18 +1,8 @@
-find_package( PkgConfig REQUIRED )
-pkg_search_module( RRDTool librrd )
-# Above line sets RRDTool_FOUND == 1 on success and
-# RRDTool_LDFLAGS to correct linker argument
-
-if( NOT RRDTool_FOUND )
-	set( RRDTOOL_FOUND FALSE CACHE INTERNAL "" FORCE )
-	return()
-else()
-	set( RRDTOOL_FOUND TRUE CACHE INTERNAL "" FORCE )
-endif()
-
 find_path( RRDTool_INCLUDE_DIR rrd.h /usr/include /usr/local/include )
 if( RRDTool_INCLUDE_DIR )
-	set( RRDTOOL_FOUND TRUE CACHE INTERNAL "" FORCE )
+	set( RRDTool_FOUND TRUE CACHE INTERNAL "" FORCE )
+	set( RRDTool_LDFLAGS "-lrrd" CACHE INTERNAL "" FORCE )
 else()
-	set( RRDTOOL_FOUND FALSE CACHE INTERNAL "" FORCE )
+	set( RRDTool_FOUND FALSE CACHE INTERNAL "" FORCE )
+	unset( RRDTool_LDFLAGS CACHE )
 endif()
